d37d653487ba1ae1e7fa904932526a4a3482bd12
Sundhar22 <mohanasundharamofficial@gmail.com>
2023-11-21 22:47:57 +0530

    conflict fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:user_app/constant/theme/app_theme.dart';
+import 'package:user_app/features/booking/ui/booking.dart';
 
-import 'package:user_app/features/booking/ui/booking_page.dart';
 import 'package:user_app/features/home/ui/home.dart';
 import 'package:user_app/features/onboarding/ui/pages/onboarding_page.dart';
 import 'package:user_app/features/onboarding/ui/pages/otp_screen.dart';
