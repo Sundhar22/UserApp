be16841d723d7181b2b2f124b302693161ee9316
Sundhar22 <mohanasundharamofficial@gmail.com>
2023-11-22 17:06:44 +0530

    complete_booking_ui

--- lib/features/booking/ui/booking.dart
+++ lib/features/booking/ui/booking.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:user_app/features/booking/ui/widgets/booking_card_widget.dart';
 import 'package:user_app/features/booking/ui/widgets/booking_widgets.dart';
 
 class BookingPage extends StatefulWidget {
@@ -22,83 +22,77 @@ class _BookingPageState extends State<BookingPage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: bookingAppBar(_tabController),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          BookingCard(),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+                BookingCard(),
+              ],
+            ),
+          ),
         ],
-      ),
-    );
-  }
-}
-
-class BookingCard extends StatelessWidget {
-  const BookingCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 0.h),
-        margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-        height: 200.h,
-        width: 330.w,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.r),
-          border: Border.all(color: Colors.grey.shade300),
-        ),
-        child: Column(
-          children: [
-            Container(
-              // padding: EdgeInsets.all(4.r),
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.grey.shade300,
-                  ),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: 'ID: ',
-                            style: TextStyle(
-                                color: Colors.black87,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.normal)),
-                        TextSpan(
-                            text: '1234567890',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.normal)),
-                      ],
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      splashFactory: NoSplash.splashFactory,
-                    ),
-                    child: Text(
-                      "View Details",
-                      style: TextStyle(
-                        fontSize: 14.5.sp,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
